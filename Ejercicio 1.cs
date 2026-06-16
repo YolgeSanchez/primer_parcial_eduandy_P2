1d4e0a0ce072df6017378302102483bd4d6f4bea
Reyson <armandofelizsantana@gmail.com>
2026-06-15 20:12:38 -0400

    Ejercicio 1 hecho gg

---
+++ Ejercicio 1.cs
@@ -0,0 +1,22 @@
+// Ejercicio 1: Sintaxis básica y variables
+
+class Ejercicio1
+{
+    static void Main(string[] args)
+    {
+        
+        Console.WriteLine("Bienvenidos al grupo: grupo-parcial");
+
+        
+        int edad = 20;
+        string nombre = "grupo-parcial";
+        double promedio = 9.5;
+        bool activo = true;
+
+       
+        Console.WriteLine("Nombre del grupo: " + nombre);
+        Console.WriteLine("Edad promedio: " + edad);
+        Console.WriteLine("Promedio de notas: " + promedio);
+        Console.WriteLine("Grupo activo: " + activo);
+    }
+}
